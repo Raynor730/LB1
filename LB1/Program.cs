de6e85a0ffbff8b2e9e0e2e081a93957dfd19db2
User <User@WIN-TF2J6GN1K4T>
2024-11-21 16:45:27 +0500

    Добавьте файлы проекта.

---
+++ LB1/Program.cs
@@ -0,0 +1,24 @@
+namespace LB1
+{
+    internal static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+            } 
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FormMain());
+        }
+    }
+}
